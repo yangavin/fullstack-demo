8447a6b0a05bd018cfc76c7c0c1253d5a4a9ea98
Spycrab-db <gavinyan2004@gmail.com>
2023-08-06 11:56:43 -0400

    Configure CORS

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
