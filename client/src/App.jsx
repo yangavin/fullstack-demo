87a43b2d34d41231d1b7909b045312c1d2670cc7
Spycrab-db <gavinyan2004@gmail.com>
2023-08-06 11:57:01 -0400

    Fetch and display data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [name, setName] = useState();
+  useEffect(() => {
+    async function getName() {
+      const res = await fetch("http://localhost:3000/");
+      setName(await res.json());
+    }
+    getName();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {name ? (
+        <h1>
+          {name.firstName} {name.lastName}
+        </h1>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
-
-export default App
